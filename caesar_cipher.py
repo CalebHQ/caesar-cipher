8445de0caf2c2b010883a349cc2daab15d442d8a
Caleb McLeod <caleb.mcleod0@gmail.com>
2021-01-10 12:04:17 +1100

    caesar cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,49 @@
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+custom = '0123456789abcdefghijklmnopqrstuvwxyz!#$%&.?@ '
+
+password = 'HardPassword'
+shift = 19
+
+
+def decrypt(password, chars):
+    print()
+    print('Decryption:')
+    decryption = ''
+    temp = ''
+    shift = len(chars)
+
+    for i in range(1, shift):
+        temp_alph = list(chars)
+        for x in range(i):
+            temp_alph.append(temp_alph[0])
+            temp_alph.pop(0)
+        shift -= 1
+
+        for letter in password.lower():
+            index = chars.index(letter)
+            temp = temp_alph[index]
+            decryption += temp
+        print(f'{shift}: {decryption.title()}')
+        decryption = ''
+
+
+def encrypt(password, shift, chars):
+    print(f'Password: {password}')
+    print(f'Shift: {shift}')
+    encryption = ''
+    temp_alph = list(chars)
+
+    for x in range(shift):
+        temp_alph.append(temp_alph[0])
+        temp_alph.pop(0)
+
+    for letter in password.lower():
+        index = chars.index(letter)
+        temp = temp_alph[index]
+        encryption += temp
+    return encryption.title()
+
+
+encryption = encrypt(password, shift, ALPHABET)
+print(f'Encryption: {encryption}')
+decrypt(encryption, ALPHABET)
